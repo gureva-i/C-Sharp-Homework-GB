91766a8fa14de3b91d91fd526f660dc9682460f9
gureva-i <gurevainna2016@gmail.com>
2023-07-15 11:56:38 +0300

    Added homework 9

---
+++ HW9/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 5 -> "2, 4"
+//M = 4; N = 8 -> "4, 6, 8"
+
+int ReadInput(string msg)
+{
+    System.Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int n = InputInt("Введите 1 положительное число");
+int m = InputInt("Введите 2 положительное число");;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
